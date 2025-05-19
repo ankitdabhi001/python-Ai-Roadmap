a4080bc375a6189b43e9bc2b661b4fb450163f92
ankitdabhi001 <a1000aje@gmail.com>
2025-05-19 12:41:59 +0530

    ok

---
+++ day2/practice.py
@@ -0,0 +1,12 @@
+no1=float(input("enter your first number : "))
+no2=float(input("enter your second number : "))
+op=input("enter your operater like(+,-,*)")
+
+if op=="+":
+    print(f"your sum is {no1+no2}")
+elif op=="-":
+     print(f"your sub is {no1-no2}")
+elif op=="*":
+     print(f"your mul is {no1*no2}")
+elif op=="/":
+     print(f"your div is {no1/no2}")

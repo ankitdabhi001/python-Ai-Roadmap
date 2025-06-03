7a6c959937919779b7577230f21644afed689644
ankitdabhi001 <a1000aje@gmail.com>
2025-06-03 11:34:37 +0530

    letest

--- day14/miniproject.py
+++ day14/miniproject.py
@@ -4,12 +4,12 @@ model_path = "./models/Llama-3.2-3B-Instruct-Q4_0.gguf"
 model = GPT4All("Llama-3.2-3B-Instruct-Q4_0.gguf", model_path="./models", allow_download=False)
 
 with model.chat_session():
-    topic = input("📝 Enter a blog topic: ")
-    prompt = f"Write a 500-word blog post about {topic}."
-    print("⏳ Generating blog post...\n")
-    blog = model.generate(prompt)
-    print("✅ Blog Generated:\n")
-    print(blog)
+    user_topic=input("enter your topic : ")
+    prompt=f"create a blog in 200 word about{user_topic}"
 
-with open(f"{topic}.txt","w") as file:
-    file.write(blog)
+    answer=model.generate(prompt)
+
+    # print(answer)
+
+with open("miniproject.txt","w") as f:
+    f.write(answer)

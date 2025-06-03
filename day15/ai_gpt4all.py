01521aeceaa4643d7767c54c97c27ffad688d97c
ankitdabhi001 <a1000aje@gmail.com>
2025-06-03 13:02:26 +0530

    neww

--- day15/ai_gpt4all.py
+++ day15/ai_gpt4all.py
@@ -5,13 +5,15 @@ from gpt4all import GPT4All
 # STEP 1: Scrape remoteok.io for job listings
 url = "https://remoteok.io/remote-dev-jobs"
 headers = {'User-Agent': 'Mozilla/5.0'}
+
 response = requests.get(url, headers=headers)
 soup = BeautifulSoup(response.text, 'html.parser')
 
 # STEP 2: Extract job titles and companies
 job_section = soup.find_all('tr', class_='job')
 
-jobs_data = []
+jobs_data = [] # empty list
+
 for job in job_section:
     title_tag = job.find('h2')
     company_tag = job.find('h3')
@@ -28,8 +30,8 @@ print("🧾 Scraped Jobs:\n")
 print(scraped_text[:500] + "\n...")  # Preview only first few lines
 
 # STEP 3: Use GPT4All (LLaMA3) to summarize
-model = GPT4All("Llama-3.2-3B-Instruct-Q4_0.gguf")
-model.open()
+model= GPT4All("Llama-3.2-3B-Instruct-Q4_0.gguf", model_path="./models", allow_download=False)
+
 
 prompt = f"""
 Here are some recent remote developer job postings:\n\n{scraped_text}
@@ -37,7 +39,7 @@ Here are some recent remote developer job postings:\n\n{scraped_text}
 Summarize the above jobs in 3-4 bullet points with main trends or roles:
 """
 
-summary = model.prompt(prompt)
+summary = model.generate(prompt)
 
 # STEP 4: Print Summary
 print("\n📌 Summary of RemoteOK Jobs:\n")
--- day15/remote_job.py
+++ day15/remote_job.py
@@ -1,24 +1,22 @@
 import requests
 from bs4 import BeautifulSoup
 
+url = "https://remoteok.io/remote-dev-jobs"
+browser = {'user-agent':'chrome'}
 
-url="https://remoteok.io/remote-dev-jobs"
-header={'user-agent':'chrome'}
+response =requests.get(url,headers=browser)
+soup = BeautifulSoup(response.text,'html.parser')
 
-response=requests.get(url,headers=header)
-soup=BeautifulSoup(response.text,'html.parser')
+job_selection=soup.find_all('tr',class_='job')
+
+with open("letest.txt","w") as f:
+
+    for a in job_selection:
+        title = a.find('h2')
+        compony = a.find('h3')
+
+        f.write(f"Job = {title.text.strip() + "\n"}")
+        f.write(f"compony = {compony.text.strip() + "\n"}")
+        f.write("---------------------------" + "\n")
 
 
-job_section= soup.find_all('tr',class_='job')
-
-
-
-with open("job.txt","w",encoding="utf-8") as f:
-
-    for job in job_section:
-        title=job.find('h2')
-        compony=job.find('h3')
-
-        if title and compony:
-            f.write(f"COMPONY : {compony.text.strip() + "\n"}")
-            f.write(f"JOb: {title.text.strip() +"\n"}")
